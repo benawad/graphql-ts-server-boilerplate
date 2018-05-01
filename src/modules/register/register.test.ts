1fbadfa4c88174df4abd2056b6f7449ceab3f1ff
ben awad <benawad97@gmail.com>
2018-05-01 14:41:59 -0500

    validate register fields

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,6 +1,12 @@
 import { request } from "graphql-request";
 import { startServer } from "../../startServer";
 import { User } from "../../entity/User";
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough
+} from "./errorMessages";
 
 let getHost = () => "";
 
@@ -13,9 +19,9 @@ beforeAll(async () => {
 const email = "tom@bob.com";
 const password = "jalksdf";
 
-const mutation = `
+const mutation = (e: string, p: string) => `
 mutation {
-  register(email: "${email}", password: "${password}") {
+  register(email: "${e}", password: "${p}") {
     path
     message
   }
@@ -23,14 +29,65 @@ mutation {
 `;
 
 test("Register user", async () => {
-  const response = await request(getHost(), mutation);
+  // make sure we can register a user
+  const response = await request(getHost(), mutation(email, password));
   expect(response).toEqual({ register: null });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
-  const response2: any = await request(getHost(), mutation);
+
+  // test for duplicate emails
+  const response2: any = await request(getHost(), mutation(email, password));
   expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual("email");
+  expect(response2.register[0]).toEqual({
+    path: "email",
+    message: duplicateEmail
+  });
+
+  // catch bad email
+  const response3: any = await request(getHost(), mutation("b", password));
+  expect(response3).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough
+      },
+      {
+        path: "email",
+        message: invalidEmail
+      }
+    ]
+  });
+
+  // catch bad password
+  const response4: any = await request(getHost(), mutation(email, "ad"));
+  expect(response4).toEqual({
+    register: [
+      {
+        path: "password",
+        message: passwordNotLongEnough
+      }
+    ]
+  });
+
+  // catch bad password and bad email
+  const response5: any = await request(getHost(), mutation("df", "ad"));
+  expect(response5).toEqual({
+    register: [
+      {
+        path: "email",
+        message: emailNotLongEnough
+      },
+      {
+        path: "email",
+        message: invalidEmail
+      },
+      {
+        path: "password",
+        message: passwordNotLongEnough
+      }
+    ]
+  });
 });
--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,16 +1,41 @@
 import * as bcrypt from "bcryptjs";
+import * as yup from "yup";
 import { ResolverMap } from "../../types/graphql-utils";
 import { User } from "../../entity/User";
+import { formatYupError } from "../../utils/formatYupError";
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  invalidEmail,
+  passwordNotLongEnough
+} from "./errorMessages";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+});
 
 export const resolvers: ResolverMap = {
   Query: {
     bye: () => "bye"
   },
   Mutation: {
-    register: async (
-      _,
-      { email, password }: GQL.IRegisterOnMutationArguments
-    ) => {
+    register: async (_, args: GQL.IRegisterOnMutationArguments) => {
+      try {
+        await schema.validate(args, { abortEarly: false });
+      } catch (err) {
+        return formatYupError(err);
+      }
+
+      const { email, password } = args;
+
       const userAlreadyExists = await User.findOne({
         where: { email },
         select: ["id"]
@@ -20,7 +45,7 @@ export const resolvers: ResolverMap = {
         return [
           {
             path: "email",
-            message: "already taken"
+            message: duplicateEmail
           }
         ];
       }
