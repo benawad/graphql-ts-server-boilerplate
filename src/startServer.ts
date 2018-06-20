153dc540dda3aca81ad990f055f95f11838531aa
Ben Awad <benawad97@gmail.com>
2018-06-20 08:18:15 -0500

    Update startServer.ts

--- src/startServer.ts
+++ src/startServer.ts
@@ -22,7 +22,7 @@ export const startServer = async () => {
   }
 
   const server = new GraphQLServer({
-    schema: genSchema(),
+    schema: genSchema() as any,
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host"),
